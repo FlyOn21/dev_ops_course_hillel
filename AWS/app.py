1136408e5f7265a1344830be6721d7fc45902b56
Pavlo Zhoholiev <p_zhoholiev@viyar.ua>
2026-01-10 21:55:48 +0200

    hw_12_containers Zhoholiev Pavlo 10_01_26 21_55 - complited make hw_12 aws cdk stack

--- AWS/app.py
+++ AWS/app.py
@@ -5,10 +5,19 @@ import aws_cdk as cdk
 
 from aws.aws_stack import EC2Stack
 
+with open(os.path.expanduser("~/.ssh/zhoholiev.pub"), "r") as f:
+    ssh_public_key = f.read().strip()
 
 app = cdk.App()
-stack = EC2Stack(app, "EC2Stack", env =cdk.Environment(region="eu-central-1"))
+stack = EC2Stack(
+    app,
+    "EC2StackHomeWork12",
+    env=cdk.Environment(region="eu-central-1"),
+    ssh_public_key=ssh_public_key,
+    allowed_ssh_ip="94.131.197.224"
+)
 cdk.Tags.of(stack).add("dev_ops", "hw12")
 cdk.Tags.of(stack).add("owner", "flyon21")
+cdk.Tags.of(stack).add("project", "ec2_cdk_project")
 
 app.synth()
--- AWS/aws/aws_stack.py
+++ AWS/aws/aws_stack.py
@@ -6,7 +6,8 @@ from constructs import Construct
 
 class EC2Stack(Stack):
 
-    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+    def __init__(self, scope: Construct, construct_id: str, ssh_public_key: str = None,
+                 allowed_ssh_ip: str = None, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
         #################VPC CREATION######################
         # Create VPC without default subnets
@@ -16,23 +17,9 @@ class EC2Stack(Stack):
             vpc_name="Flyon21VPC",
             ip_addresses=ec2.IpAddresses.cidr("192.168.0.0/16"),
             max_azs=1,
-            nat_gateways=0,  # We'll create manually
-            subnet_configuration=[]  # No default subnets
-        )
-
-        # Create Internet Gateway
-        internet_gateway = ec2.CfnInternetGateway(
-            self,
-            "InternetGatewayFlyon21HW12",
-            tags=[{"key": "name", "value": "Flyon21-IGW"}]
-        )
-
-        # Attach Internet Gateway to VPC
-        ec2.CfnVPCGatewayAttachment(
-            self,
-            "IGWAttachmentFlyon21HW12",
-            vpc_id=vpc.vpc_id,
-            internet_gateway_id=internet_gateway.ref
+            create_internet_gateway=False,
+            nat_gateways=0,
+            subnet_configuration=[]
         )
 
         # Create Public Subnet
@@ -54,6 +41,22 @@ class EC2Stack(Stack):
             cidr_block="192.168.1.0/24"
         )
 
+        # Create Internet Gateway
+        internet_gateway = ec2.CfnInternetGateway(
+            self,
+            "InternetGatewayFlyon21HW12",
+            tags=[{"key": "name", "value": "Flyon21-IGW"}]
+        )
+
+        # Attach Internet Gateway to VPC
+        igw_attachment = ec2.CfnVPCGatewayAttachment(
+            self,
+            "IGWAttachmentFlyon21HW12",
+            vpc_id=vpc.vpc_id,
+            internet_gateway_id=internet_gateway.ref
+        )
+        igw_attachment.add_dependency(internet_gateway)
+
         # Create Elastic IP for NAT Gateway
         elastic_ip = ec2.CfnEIP(
             self,
@@ -70,6 +73,7 @@ class EC2Stack(Stack):
             allocation_id=elastic_ip.attr_allocation_id,
             tags=[{"key": "name", "value": "Flyon21-NAT"}]
         )
+        nat_gateway.add_dependency(elastic_ip)
 
         # Create Route Table for Public Subnet
         public_route_table = ec2.CfnRouteTable(
@@ -80,21 +84,24 @@ class EC2Stack(Stack):
         )
 
         # Add route to Internet Gateway in Public Route Table
-        ec2.CfnRoute(
+        public_route = ec2.CfnRoute(
             self,
             "PublicRouteFlyon21HW12",
             route_table_id=public_route_table.ref,
             destination_cidr_block="0.0.0.0/0",
             gateway_id=internet_gateway.ref
         )
+        public_route.add_dependency(igw_attachment)
+        public_route.add_dependency(public_route_table)
 
         # Associate Public Subnet with Public Route Table
-        ec2.CfnSubnetRouteTableAssociation(
+        assosiate_publick_subnet = ec2.CfnSubnetRouteTableAssociation(
             self,
             "PublicSubnetRTAssociationFlyon21HW12",
             subnet_id=public_subnet.subnet_id,
             route_table_id=public_route_table.ref
         )
+        assosiate_publick_subnet.add_dependency(public_route_table)
 
         # Create Route Table for Private Subnet
         private_route_table = ec2.CfnRouteTable(
@@ -105,23 +112,129 @@ class EC2Stack(Stack):
         )
 
         # Add route to NAT Gateway in Private Route Table
-        ec2.CfnRoute(
+        private_route= ec2.CfnRoute(
             self,
             "PrivateRouteFlyon21HW12",
             route_table_id=private_route_table.ref,
             destination_cidr_block="0.0.0.0/0",
             nat_gateway_id=nat_gateway.ref
         )
+        private_route.add_dependency(nat_gateway)
 
         # Associate Private Subnet with Private Route Table
-        ec2.CfnSubnetRouteTableAssociation(
+        assosiate_private = ec2.CfnSubnetRouteTableAssociation(
             self,
             "PrivateSubnetRTAssociationFlyon21HW12",
             subnet_id=private_subnet.subnet_id,
             route_table_id=private_route_table.ref
         )
+        assosiate_private.add_dependency(private_route_table)
+
+        #################SSH KEY PAIR######################
+        key_pair_kwargs = {
+            "key_pair_name": "Flyon21-KeyPair"
+        }
+
+        if ssh_public_key:
+            key_pair_kwargs["public_key_material"] = ssh_public_key
+
+        key_pair = ec2.KeyPair(
+            self,
+            "EC2KeyPairFlyon21HW12",
+            **key_pair_kwargs
+        )
+
+        #################SECURITY GROUPS######################
+        # Security Group for Public EC2 Instance
+        public_security_group = ec2.SecurityGroup(
+            self,
+            "PublicSecurityGroupFlyon21HW12",
+            vpc=vpc,
+            security_group_name="Flyon21-Public-SG",
+            description="Security group for public EC2 instance (Bastion) - allows SSH from your IP only",
+            allow_all_outbound=True
+        )
+
+        # Add SSH ingress rule from your IP address
+        if allowed_ssh_ip:
+            public_security_group.add_ingress_rule(
+                peer=ec2.Peer.ipv4(f"{allowed_ssh_ip}/32"),
+                connection=ec2.Port.tcp(22),
+                description=f"Allow SSH from your IP: {allowed_ssh_ip}"
+            )
+        else:
+            public_security_group.add_ingress_rule(
+                peer=ec2.Peer.any_ipv4(),
+                connection=ec2.Port.tcp(22),
+                description="Allow SSH from anywhere. !!!CHANGE THIS!!!"
+            )
+
+        # Security Group for Private EC2 Instance
+        private_security_group = ec2.SecurityGroup(
+            self,
+            "PrivateSecurityGroupFlyon21HW12",
+            vpc=vpc,
+            security_group_name="Flyon21-Private-SG",
+            description="Security group for private EC2 instance - allows SSH from public subnet only",
+            allow_all_outbound=True # Allow outbound traffic
+        )
+
+        # Add SSH ingress rule from Public Security Group
+        private_security_group.add_ingress_rule(
+            peer=ec2.Peer.security_group_id(public_security_group.security_group_id),
+            connection=ec2.Port.tcp(22),
+            description="Allow SSH from bastion host security group"
+        )
+
+        #################EC2 INSTANCES######################
+        # Get the latest Amazon Linux AMI
+        amzn_linux = ec2.MachineImage.latest_amazon_linux2(
+            kernel=ec2.AmazonLinux2Kernel.KERNEL_5_10,
+            edition=ec2.AmazonLinuxEdition.STANDARD,
+            virtualization=ec2.AmazonLinuxVirt.HVM,
+            storage=ec2.AmazonLinuxStorage.GENERAL_PURPOSE,
+            cpu_type=ec2.AmazonLinuxCpuType.X86_64 # or ARM_64
+        )
+
+        # Create EC2 Instance in Public Subnet
+        public_instance = ec2.Instance(
+            self,
+            "PublicEC2InstanceFlyon21HW12",
+            instance_type=ec2.InstanceType("t2.micro"),
+            machine_image=amzn_linux,
+            vpc=vpc,
+            vpc_subnets=ec2.SubnetSelection(subnets=[public_subnet]),
+            security_group=public_security_group,
+            key_pair=key_pair,
+            instance_name="Flyon21-Public-Instance",
+            private_ip_address="192.168.0.4"
+        )
+
+        # Create Elastic IP for Public Instance for SSH Access
+        public_eip = ec2.CfnEIP(
+            self,
+            "PublicInstanceEIPFlyon21HW12",
+            domain="vpc",
+            instance_id=public_instance.instance_id,
+            tags=[{"key": "name", "value": "Flyon21-Public-Instance-EIP"}]
+        )
+
+        # Create EC2 Instance in Private Subnet
+        private_instance = ec2.Instance(
+            self,
+            "PrivateEC2InstanceFlyon21HW12",
+            instance_type=ec2.InstanceType("t2.micro"),
+            machine_image=amzn_linux,
+            vpc=vpc,
+            vpc_subnets=ec2.SubnetSelection(subnets=[private_subnet]),
+            security_group=private_security_group,
+            key_pair=key_pair,
+            instance_name="Flyon21-Private-Instance",
+            private_ip_address="192.168.1.4"
+        )
 
         #################OUTPUTS######################
+        # Output VPC ID
         CfnOutput(
             self,
             "VPCIdOutput",
@@ -129,6 +242,8 @@ class EC2Stack(Stack):
             description="The ID of the VPC",
             export_name="VPCIdFlyon21HW12"
         )
+
+        # Output Public Subnet ID
         CfnOutput(
             self,
             "PublicSubnetIdOutput",
@@ -136,6 +251,8 @@ class EC2Stack(Stack):
             description="The ID of the Public Subnet",
             export_name="PublicSubnetIdFlyon21HW12"
         )
+
+        # Output Private Subnet ID
         CfnOutput(
             self,
             "PrivateSubnetIdOutput",
@@ -143,6 +260,8 @@ class EC2Stack(Stack):
             description="The ID of the Private Subnet",
             export_name="PrivateSubnetIdFlyon21HW12"
         )
+
+        # Output Internet Gateway ID
         CfnOutput(
             self,
             "InternetGatewayIdOutput",
@@ -150,10 +269,82 @@ class EC2Stack(Stack):
             description="The ID of the Internet Gateway",
             export_name="InternetGatewayIdFlyon21HW12"
         )
+
+        # Output NAT Gateway ID
         CfnOutput(
             self,
             "NATGatewayIdOutput",
             value=nat_gateway.ref,
             description="The ID of the NAT Gateway",
             export_name="NATGatewayIdFlyon21HW12"
+        )
+
+        # Output Key Pair Name
+        CfnOutput(
+            self,
+            "KeyPairNameOutput",
+            value=key_pair.key_pair_name,
+            description="The name of the SSH Key Pair",
+            export_name="KeyPairNameFlyon21HW12"
+        )
+
+        # Output Public EC2 Instance ID
+        CfnOutput(
+            self,
+            "PublicInstanceIdOutput",
+            value=public_instance.instance_id,
+            description="The ID of the Public EC2 Instance",
+            export_name="PublicInstanceIdFlyon21HW12"
+        )
+
+        # Output Public EC2 Instance Private IP
+        CfnOutput(
+            self,
+            "PublicInstancePrivateIpOutput",
+            value=public_instance.instance_private_ip,
+            description="The Private IP of the Public EC2 Instance",
+            export_name="PublicInstancePrivateIpFlyon21HW12"
+        )
+
+        # Output Public EC2 Instance Public IP (EIP - Stable)
+        CfnOutput(
+            self,
+            "PublicInstancePublicIpOutput",
+            value=public_eip.ref,
+            description="The Public IP (EIP) of the Public EC2 Instance - stable, doesn't change on restart",
+            export_name="PublicInstancePublicIpFlyon21HW12"
+        )
+
+        # Output Private EC2 Instance ID
+        CfnOutput(
+            self,
+            "PrivateInstanceIdOutput",
+            value=private_instance.instance_id,
+            description="The ID of the Private EC2 Instance",
+            export_name="PrivateInstanceIdFlyon21HW12"
+        )
+
+        # Output Private EC2 Instance Private IP
+        CfnOutput(
+            self,
+            "PrivateInstancePrivateIpOutput",
+            value=private_instance.instance_private_ip,
+            description="The Private IP of the Private EC2 Instance",
+            export_name="PrivateInstancePrivateIpFlyon21HW12"
+        )
+
+        # Output Security Group IDs
+        CfnOutput(
+            self,
+            "PublicSecurityGroupIdOutput",
+            value=public_security_group.security_group_id,
+            description="The ID of the Public Security Group",
+            export_name="PublicSecurityGroupIdFlyon21HW12"
+        )
+        CfnOutput(
+            self,
+            "PrivateSecurityGroupIdOutput",
+            value=private_security_group.security_group_id,
+            description="The ID of the Private Security Group",
+            export_name="PrivateSecurityGroupIdFlyon21HW12"
         )
